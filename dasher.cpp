d250f8587116fc5dbc2ff05dd8fcfdfcce2edf3e
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-10-06 14:38:25 -0300

    Add file associations for .m files and update player movement

--- dasher.cpp
+++ dasher.cpp
@@ -6,9 +6,17 @@ int main()
   const int gameHeight{720};
   InitWindow(gameWidth, gameHeight, "Dapper Dasher!");
   SetTargetFPS(60);
+
+  const int width{50};
+  const int height={80};
+  
+  int posY{gameHeight - height};
+  int velocity{-10};
+  
   while(!WindowShouldClose()) 
   {
     BeginDrawing();
+    posY += velocity;
     ClearBackground(RAYWHITE);
     DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
     EndDrawing();
