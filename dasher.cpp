c7720cb3d891017a820a3ddc17252cbfae53cc25
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-10-15 13:50:39 -0300

    Add gravity and jumping mechanics

--- dasher.cpp
+++ dasher.cpp
@@ -21,37 +21,49 @@ typedef struct Game
 Game game;
 Player player;
 const int gravity = 1;
+const int groundPosition = game.height - player.height;
 
 void placePlayer() 
 {
   player.posX = game.width / 2 - player.width / 2;
-  player.posY = game.height - player.height;
+  player.posY = groundPosition;
 }
 
+void applyGravity()
+{
+  if(player.posY != groundPosition)
+  {
+    player.velocityY += gravity;
+  }
+}
+void handleJump()
+{
+  bool isJumping = IsKeyDown(KEY_SPACE);
+  if(isJumping && player.posY == groundPosition) 
+  {
+    player.velocityY = -20;
+  }
+}
+void fallToTheGround()
+{
+  player.posY += player.velocityY;
+  if(player.posY > groundPosition) 
+  {
+    player.posY = groundPosition;
+    player.velocityY = 0;
+  }
+}
 int main() 
 {
   InitWindow(game.width, game.height, game.title);
   SetTargetFPS(game.FPS);
   placePlayer();
-  const int groundPosition = player.posY;
-
   while(!WindowShouldClose()) 
   {
     BeginDrawing();
-    if(player.posY != groundPosition){
-      player.velocityY += gravity;
-    }
-    bool isJumping = IsKeyDown(KEY_SPACE);
-    if(isJumping && player.posY == groundPosition) 
-    {
-      player.velocityY = -20;
-    }
-    player.posY += player.velocityY;
-    if(player.posY > groundPosition) 
-    {
-      player.posY = groundPosition;
-      player.velocityY = 0;
-    }
+    applyGravity();
+    handleJump();
+    fallToTheGround();
     DrawRectangle(player.posX, player.posY, player.width, player.height, RED);
     ClearBackground(RAYWHITE);
     EndDrawing();
