276b4e4732d99e73a51656ea5c4120cd7d504f3b
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-10-15 13:31:13 -0300

    Add player struct and game struct

--- dasher.cpp
+++ dasher.cpp
@@ -1,29 +1,61 @@
 #include <raylib.h>
 
+typedef struct Player 
+{
+  int posX = 0;
+  int posY = 0;
+  int velocityX = 0;
+  int velocityY = 0;
+  const int width = 50;
+  const int height = 80;
+} Player;
+
+typedef struct Game 
+{
+  const int width = 1024;
+  const int height = 720;
+  const int FPS = 60;
+  const char* title = "Dapper Dasher!";
+} Game;
+
+Game game;
+Player player;
+const int gravity = 1;
+
+void placePlayer() 
+{
+  player.posX = game.width / 2 - player.width / 2;
+  player.posY = game.height - player.height;
+}
+
 int main() 
 {
-  const int gameWidth{1024};
-  const int gameHeight{720};
-  InitWindow(gameWidth, gameHeight, "Dapper Dasher!");
-  SetTargetFPS(60);
-
-  const int width{50};
-  const int height={80};
-  
-  int posY{gameHeight - height};
-  int velocity{-10};
+  InitWindow(game.width, game.height, game.title);
+  SetTargetFPS(game.FPS);
+  placePlayer();
+  const int groundPosition = player.posY;
 
   while(!WindowShouldClose()) 
   {
     BeginDrawing();
-    bool isJumping = IsKeyDown(KEY_SPACE);
-    if(isJumping) 
-    {
-      posY += velocity;
+    if(player.posY != groundPosition){
+      player.velocityY += gravity;
     }
-    DrawRectangle(gameWidth / 2 - width / 2, posY, width, height, RED);
+    bool isJumping = IsKeyDown(KEY_SPACE);
+    if(isJumping && player.posY == groundPosition) 
+    {
+      player.velocityY = -20;
+    }
+    player.posY += player.velocityY;
+    if(player.posY > groundPosition) 
+    {
+      player.posY = groundPosition;
+      player.velocityY = 0;
+    }
+    DrawRectangle(player.posX, player.posY, player.width, player.height, RED);
     ClearBackground(RAYWHITE);
     EndDrawing();
   }
   CloseWindow();
 }
+
