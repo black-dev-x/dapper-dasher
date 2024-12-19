331fca384d3f3b393afcccc37fd9448d6b9b2f3d
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-19 18:24:43 -0300

    Refactor player struct and add sprite animation

--- dasher.cpp
+++ dasher.cpp
@@ -1,13 +1,12 @@
 #include <raylib.h>
-
+#include <stdio.h>
 typedef struct Player 
 {
-  int posX = 0;
-  int posY = 0;
-  int velocityX = 0;
-  int velocityY = 0;
-  const int width = 50;
-  const int height = 80;
+  Vector2 pos;
+  Texture2D sprite;
+  Vector2 velocity;
+  Rectangle avatar;
+  int groundPosition;
 } Player;
 
 typedef struct Game 
@@ -16,58 +15,79 @@ typedef struct Game
   const int height = 720;
   const int FPS = 60;
   const char* title = "Dapper Dasher!";
+  float deltaTime;
+  float runningTime;
 } Game;
 
 Game game;
 Player player;
-const int gravity = 1;
-const int groundPosition = game.height - player.height;
+const int gravity = 1000;
+
+void initialize()
+{
+  player.sprite = LoadTexture("textures/scarfy.png");
+  player.avatar.width = (float) player.sprite.width / 6;
+  player.avatar.height = player.sprite.height;
+  player.groundPosition = game.height - player.avatar.height;
+}
 
 void placePlayer() 
 {
-  player.posX = game.width / 2 - player.width / 2;
-  player.posY = groundPosition;
+  player.pos.x = game.width / 2 - player.avatar.width / 2;
+  player.pos.y = player.groundPosition;
 }
 
 void applyGravity()
 {
-  if(player.posY != groundPosition)
+  if(player.pos.y != player.groundPosition)
   {
-    player.velocityY += gravity;
+    player.velocity.y += gravity * game.deltaTime;
   }
 }
 void handleJump()
 {
   bool isJumping = IsKeyDown(KEY_SPACE);
-  if(isJumping && player.posY == groundPosition) 
+  if(isJumping && player.pos.y == player.groundPosition) 
   {
-    player.velocityY = -20;
+    player.velocity.y = -600;
   }
 }
 void fallToTheGround()
 {
-  player.posY += player.velocityY;
-  if(player.posY > groundPosition) 
+  player.pos.y += player.velocity.y * game.deltaTime;
+  if(player.pos.y > player.groundPosition) 
   {
-    player.posY = groundPosition;
-    player.velocityY = 0;
+    player.pos.y = player.groundPosition;
+    player.velocity.y = 0;
   }
+}
+void run()
+{
+  float updateTime = 1.0 / 12; //update 1 times per second
+  game.runningTime += game.deltaTime;
+  float cycles = game.runningTime / updateTime;
+  float currentSprite = (int)cycles % 6;
+  player.avatar.x = currentSprite * player.avatar.width;
 }
 int main() 
 {
   InitWindow(game.width, game.height, game.title);
+  initialize();
   SetTargetFPS(game.FPS);
   placePlayer();
   while(!WindowShouldClose()) 
-  {
+  {  
+    game.deltaTime = GetFrameTime();
     BeginDrawing();
     applyGravity();
     handleJump();
+    run();
     fallToTheGround();
-    DrawRectangle(player.posX, player.posY, player.width, player.height, RED);
+    DrawTextureRec(player.sprite, player.avatar, player.pos, WHITE);
     ClearBackground(RAYWHITE);
     EndDrawing();
   }
+  UnloadTexture(player.sprite);
   CloseWindow();
 }
 
